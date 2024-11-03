9f4104c1461e48996e1603fee2cbcf8ed1a703ce
ata-kaner <ata.kaner03@gmail.com>
2024-11-03 17:22:18 +0300

    r8q: Import audio configs
    Change-Id: I1a24dbd6d892a98ecd925c0b3960f9ac54b402e0

--- device.mk
+++ device.mk
@@ -11,6 +11,11 @@ $(call inherit-product, device/samsung/sm8250-common/common.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info_diff.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_diff.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Init files
 PRODUCT_PACKAGES += \
     init.r8q.rc
